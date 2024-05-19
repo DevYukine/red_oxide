c096714316adf9c95322ecce89420116e3fa1cac
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-05-19 09:21:15 +0100

    refactor(ext_deps): remove redundant return

--- src/ext_deps/util.rs
+++ src/ext_deps/util.rs
@@ -1,41 +1,41 @@
 // Windows
 #[cfg(target_os = "windows")]
 pub fn get_flac_executable() -> String {
-    return "flac.exe".to_string();
+    "flac.exe".to_string()
 }
 
 #[cfg(target_os = "windows")]
 pub fn get_sox_executable() -> String {
-    return "sox.exe".to_string();
+    "sox.exe".to_string()
 }
 
 #[cfg(target_os = "windows")]
 pub fn get_lame_executable() -> String {
-    return "lame.exe".to_string();
+    "lame.exe".to_string()
 }
 
 #[cfg(target_os = "windows")]
 pub fn get_imdl_executable_name() -> String {
-    return "imdl.exe".to_string();
+    "imdl.exe".to_string()
 }
 
 // Linux or Mac
 #[cfg(not(target_os = "windows"))]
 pub fn get_flac_executable() -> String {
-    return "flac".to_string();
+    "flac".to_string()
 }
 
 #[cfg(not(target_os = "windows"))]
 pub fn get_sox_executable() -> String {
-    return "sox".to_string();
+    "sox".to_string()
 }
 
 #[cfg(not(target_os = "windows"))]
 pub fn get_lame_executable() -> String {
-    return "lame".to_string();
+    "lame".to_string()
 }
 
 #[cfg(not(target_os = "windows"))]
 pub fn get_imdl_executable_name() -> String {
-    return "imdl".to_string();
+    "imdl".to_string()
 }
