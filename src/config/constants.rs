fdb21a5f317414460d758535b5ff1fa611208b2f
Yukine <devyukine@gmx.de>
2024-11-24 16:55:44 +0100

    fix(windows): add windows path constants back

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -9,3 +9,6 @@ pub const XDG_CONFIG_ENV: &str = "XDG_CONFIG_HOME";
 pub const HOME_ENV: &str = "HOME";
 
 pub const WINDOWS_APPDATA_ENV: &str = "APPDATA";
+pub const WINDOWS_USERPROFILE_ENV: &str = "USERPROFILE";
+pub const WINDOWS_HOMEDRIVE_ENV: &str = "HOMEDRIVE";
+pub const WINDOWS_HOMEPATH_ENV: &str = "HOMEPATH";
