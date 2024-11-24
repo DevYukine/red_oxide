2400938d0f6e4a9238908fe7949375ca44caa329
Yukine <devyukine@gmx.de>
2024-11-24 16:57:32 +0100

    feat(constants): only compile windows constants on windows os

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -9,6 +9,10 @@ pub const XDG_CONFIG_ENV: &str = "XDG_CONFIG_HOME";
 pub const HOME_ENV: &str = "HOME";
 
 pub const WINDOWS_APPDATA_ENV: &str = "APPDATA";
+
+#[cfg(target_os = "windows")]
 pub const WINDOWS_USERPROFILE_ENV: &str = "USERPROFILE";
+#[cfg(target_os = "windows")]
 pub const WINDOWS_HOMEDRIVE_ENV: &str = "HOMEDRIVE";
+#[cfg(target_os = "windows")]
 pub const WINDOWS_HOMEPATH_ENV: &str = "HOMEPATH";
